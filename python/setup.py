8c892ed1b03a88c67bd6980c476521b301760e0c
Bu Sun Kim <busunkim@google.com>
2021-01-27 00:46:47 +0000

    Exclusively use pkgutil style namespace package

--- python/setup.py
+++ python/setup.py
@@ -280,7 +280,6 @@ if __name__ == '__main__':
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         ],
-      namespace_packages=['google'],
       packages=find_packages(
           exclude=[
               'import_test_package',
